09bbd5c54d40f1710ab522d3255e3dd02688dc9e
rafavanzele <rafa_vanzele@hotmail.com>
2024-09-17 16:54:47 -0300

    resgatando dado pelo API do github

--- github_finder/src/routes/Home.tsx
+++ github_finder/src/routes/Home.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { UserProps } from "../types/user"
+
+import Search from "../components/Search"
+
+import { useState } from "react"
 
 const Home = () => {
+    const [user, setUser] = useState<UserProps | null>(null)
+
+    const loadUser = async(userName: string) => {
+
+        const res = await fetch(`https://api.github.com/users/${userName}`)
+
+        const data = await res.json()
+
+        console.log(data)
+
+    }
+
+
   return (
-    <div>Home</div>
+    <Search loadUser={loadUser}/>
   )
 }
 
